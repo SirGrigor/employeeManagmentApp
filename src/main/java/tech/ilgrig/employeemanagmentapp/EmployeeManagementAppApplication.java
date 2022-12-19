c345c286840fefea4814eb5e1666bee838fe98de
sirGrigor <grigorjev.ilja@gmail.com>
2022-12-19 23:14:21 +0200

    Initial commit

---
+++ src/main/java/tech/ilgrig/employeemanagmentapp/EmployeeManagementAppApplication.java
@@ -0,0 +1,13 @@
+package tech.ilgrig.employeemanagmentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagmentAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagmentAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/tech/ilgrig/employeemanagmentapp/model/Employee.java
@@ -0,0 +1,2 @@
+package tech.ilgrig.employeemanagmentapp.model;public class Employee {
+}
