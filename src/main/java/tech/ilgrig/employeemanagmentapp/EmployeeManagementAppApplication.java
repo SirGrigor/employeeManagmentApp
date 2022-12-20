1147645487eed20251d7b33e863acff545aa6d63
sirGrigor <grigorjev.ilja@gmail.com>
2022-12-20 11:47:22 +0200

    feature/development: added Employee entity, updated pom and refactored Application startup

--- src/main/java/tech/ilgrig/employeemanagmentapp/EmployeeManagementAppApplication.java
+++ src/main/java/tech/ilgrig/employeemanagmentapp/EmployeeManagementAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeManagmentAppApplication {
+public class EmployeeManagementAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeeManagmentAppApplication.class, args);
+        SpringApplication.run(EmployeeManagementAppApplication.class, args);
     }
 
 }
--- src/main/java/tech/ilgrig/employeemanagmentapp/model/Employee.java
+++ src/main/java/tech/ilgrig/employeemanagmentapp/model/Employee.java
@@ -1,2 +1,100 @@
-package tech.ilgrig.employeemanagmentapp.model;public class Employee {
+package tech.ilgrig.employeemanagmentapp.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "Employee")
+@Table(name = "employee")
+public class Employee implements Serializable {
+    @Id
+    @SequenceGenerator(
+            name = "employee_sequence",
+            sequenceName = "employee_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "employee_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false,
+            nullable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "first_name",
+            updatable = true
+    )
+    private String firstName;
+
+    @Column(
+            name = "last_name",
+            updatable = true
+    )
+    private String lastName;
+
+    @Column(
+            name = "email",
+            updatable = true,
+            nullable = false
+    )
+    private String email;
+
+    @Column(
+            name = "image_url",
+            updatable = true
+    )
+    private String imageUrl;
+
+    @Column(
+            name = "title",
+            updatable = true
+    )
+
+    private String title;
+    @Column(
+            name = "phone",
+            updatable = true
+    )
+    private String phone;
+
+    @Column(
+            name = "employee_code",
+            updatable = false,
+            nullable = false
+    )
+    private String employeeCode;
+
+    public Employee(String firstName, String lastName, String email, String imageUrl, String title, String phone, String employeeCode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.imageUrl = imageUrl;
+        this.title = title;
+        this.phone = phone;
+        this.employeeCode = employeeCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", title='" + title + '\'' +
+                ", phone='" + phone + '\'' +
+                ", employeeCode='" + employeeCode + '\'' +
+                '}';
+    }
 }
