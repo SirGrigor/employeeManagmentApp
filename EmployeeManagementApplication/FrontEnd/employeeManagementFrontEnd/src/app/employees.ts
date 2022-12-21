e2da00748e78399ef2e0ce9cec88ca66414073ad
sirGrigor <grigorjev.ilja@gmail.com>
2022-12-21 20:42:05 +0200

    feature/frontend - angular application init

---
+++ EmployeeManagementApplication/FrontEnd/employeeManagementFrontEnd/src/app/employees.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+  id: number;
+  firstName: string;
+
+  lastName: string;
+  email: string;
+  title: string;
+  phone: string;
+  imageUrl: string;
+  employeeCode: string;
+}
